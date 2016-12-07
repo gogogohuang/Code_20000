08ecd2245ec855c5bd0e44faaae01d8c05dbf859
{gogogohuang} <{gogogohuang@gmail.com}>
2016-12-07 23:52:38 +0800

    git diff layout.jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Code-154/20000' });
+    //res.send('<h1>Fuck you</h1>');
 });
 
 module.exports = router;
