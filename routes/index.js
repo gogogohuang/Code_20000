83ef756d881c8872502da1a45e2d9fae1e6cc53c
{gogogohuang} <{gogogohuang@gmail.com}>
2016-12-08 00:49:42 +0800

    index page

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    //res.sendFile(path.join(__dirname + '/views/index.html'));
     res.render('index', { title: 'Code-154/20000' });
-    //res.send('<h1>Fuck you</h1>');
 });
 
 module.exports = router;
