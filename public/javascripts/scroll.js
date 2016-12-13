c1b89636ab33594a43510abfcd2ac80bf1780507
{gogogohuang} <{gogogohuang@gmail.com}>
2016-12-13 10:43:44 +0800

    index-2

---
+++ public/javascripts/scroll.js
@@ -0,0 +1,28 @@
+/*
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+        e.preventDefault()
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear')
+    })
+});
+*/
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        console.log("page-scroll");
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
